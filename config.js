93a65c8d564bf61be99b1339bad238e2d1a9514d
Razul Mhd Alfhika Sadisna <68502508+koncit@users.noreply.github.com>
2022-06-27 02:41:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,43 +1,43 @@
 //=============『 Utama 』================== //
-global.owner = ['6282138919347'] 
+global.owner = ['6281261973803'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Helga Zexs'
-global.numberowner = '6282138919347'
-global.instagram = 'https://instagram.com/helga_store20'
-global.github = 'https://github.com/Hyzerr'
-global.dana = '081398860278'
-global.pulsa = '082138919347'
-global.gopay = '081398860278'
+global.nameowner = 'Razul'
+global.numberowner = '6281261973803'
+global.instagram = 'https://instagram.com/k0nc1t'
+global.github = 'https://github.com/koncit'
+global.dana = '081261973803'
+global.pulsa = '081261973803'
+global.gopay = '081261973803'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'ShiroBot-Md'
-global.gc = 'https://chat.whatsapp.com/BxrDMvfLeKTHtvAWKHWZbZ'
-global.web = 'https://www.helgastoree.xyz' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'BearBot-Md'
+global.gc = 'https://chat.whatsapp.com/KZ8a8elMR1lHfKoW7kNNto'
+global.web = 'https://www.app-koncit.herokuapp.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/94ba03b80edd8f8a8ae75.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/94ba03b80edd8f8a8ae75.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = 'Zuka Beta'
+global.wm = 'Bear Bot'
 global.watermark = wm
-global.wm2 = '                     「 Zuka Beta あ⁩ 」'
-global.wm3 = '⫹⫺ Zuka Beta'
+global.wm2 = '                     「 Bear Bot あ⁩ 」'
+global.wm3 = '⫹⫺ Bear Bot'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.media = 'https://telegra.ph/file/94ba03b80edd8f8a8ae75.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = '2022'
-global.author = '© Helga Zexs'
+global.packname = 'Bear Bot'
+global.author = '© Razul Ganteng Xixixi'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
